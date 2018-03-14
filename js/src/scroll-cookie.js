cbb40d3156738ecee30c01ad06048f7685df4d82
tang <tangxianmeng@live.com>
2018-03-14 15:39:44 +0800

    Site updated: 2018-03-14 15:39:16

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 09 2018 10:54:17 GMT+0800 (China Standard Time)
+// build time:Wed Mar 14 2018 15:39:03 GMT+0800 (China Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
